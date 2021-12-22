1db772c4146443445032cc5ea1685299f475a136
Ana <anafvana@gmail.com>
2021-12-22 23:57:56 +0100

    📦 NEW: Added symlink creation to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-# basic setup packages
-sudo apt install bspwm sxhkd dunst brightnessctl alsa-utils acpi scrot alacritty
+DOTFILES=$(echo `pwd` | sed "s/\/scripts//")
+SCRIPTS=$(echo `pwd`)
 
-# allow brightness changes without sudo
-sudo chown root:root /usr/bin/brightnesctl
-sudo chmod 4775 /usr/bin/brightnessctl
+# PACKAGES
+## Basic setup packages
+#sudo apt install bspwm sxhkd dunst brightnessctl alsa-utils acpi scrot alacritty
 
-# additional setup packages
-sudo apt install insync tree silversearcher-ag
+## Additional setup packages
+#sudo apt install insync tree silversearcher-ag
+
+# SETTINGS
+## Brightness without sudo
+#sudo chown root:root /usr/bin/brightnesctl
+#sudo chmod 4775 /usr/bin/brightnessctl
+
+# SYMLINKS
+## bashrc
+ln -s $DOTFILES/.bashrc $HOME
+
+## .config files
+# format: targetFile directory
+SYMLINKS=(
+	"bspwmrc" ".config/bspwm"
+	"sxhkdrc" ".config/sxhkd"
+	"nvim" ".config/nvim"
+)
+
+for (( i=0;i<${#SYMLINKS[@]};i++))
+do
+	TARGET=$DOTFILES/${SYMLINKS[$i]}
+	i=$(($i+1))
+	DIRECT=$HOME/${SYMLINKS[$i]}
+
+	chmod +x "$TARGET"
+	if [[ ! -d "$DIRECT" ]]
+	then
+		mkdir -p "$DIRECT"
+	fi
+	ln -s "$TARGET" "$DIRECT"
+done
+
+## Touchpad config
+sudo ln -s $DOTFILES/xorg.conf.d/30-touchpad.conf /usr/share/X11/xorg.conf.d
