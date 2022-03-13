67be054320f681fb6bda6af17c35c0a32df23a8e
Ana <anafvana@gmail.com>
2022-03-13 19:26:34 +0100

    👌 IMPROVE: Notify-sysinfo handles when identifying 2 batteries

--- scripts/notify-sysinfo.sh
+++ scripts/notify-sysinfo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-notify-send -t 2000 -h string:x-canonical-private-synchronous:sysinfo "Sysinfo" "\n`date +"%A %d %B %Y %H:%M:%S"`\n`acpi |
-grep "Battery\ 0\:\ " |
-sed "s/Battery\ 0\:\ //g"`"
+notify-send -t 2000 -h string:x-canonical-private-synchronous:sysinfo "Sysinfo" "\n$(date +"%A %d %B %Y %H:%M:%S")\n$(acpi |
+grep "harging" |
+sed "s/Battery\ 0\:\ //g" |
+sed "s/Battery\ 1\:\ //g")"
