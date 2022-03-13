f0a3faf9cc55c3ef399182900e7cb1aa77240cce
Ana <anafvana@gmail.com>
2022-03-14 00:00:27 +0100

    🤏 MINOR: Added 'Full' case for notify-sysinfo

--- scripts/notify-sysinfo.sh
+++ scripts/notify-sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 notify-send -t 2000 -h string:x-canonical-private-synchronous:sysinfo "Sysinfo" "\n$(date +"%A %d %B %Y %H:%M:%S")\n$(acpi |
-grep "harging" |
+grep -e "harging" -e "Full" |
 sed "s/Battery\ 0\:\ //g" |
 sed "s/Battery\ 1\:\ //g")"
