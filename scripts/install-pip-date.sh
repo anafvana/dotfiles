0d4bbe9a675072af963ade9676ccfe513e271aad
Ana <anafvana@gmail.com>
2024-06-22 02:23:21 +0200

    🔀 MERGE: mac -> macwork

---
+++ scripts/install-pip-date.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if (( $# != 1 ))
+then
+    >&2 echo "Illegal number of arguments. Must be exactly 1"
+	exit 1
+fi
+
+pkg=$1
+pkgExists=`echo $(type "$pkg" 2>&1)`
+
+
+if ! [[ $pkgExists =~ "not found" ]]
+then
+	echo "$pkg is already installed"
+	exit 0
+fi
+
+#pip install "$pkg"
+
+answer="none"
+
+while [[ true ]]
+do
+	case "$answer" in
+		"n"|"no"|"")
+			break ;;
+		"y"|"yes")
+			break ;;
+		*)
+			read -r -p "Do you want to link this package's binaries to /usr/local/bin ? [y]es [N]o: " answer
+			answer=$(echo "$answer" | awk '{print tolower($0)}') ;;
+	esac
+done
+
+echo "\"$answer\""
---
+++ scripts/mac-setup.sh
@@ -0,0 +1,5 @@
+brew update
+brew tap FelixKratz/formulae
+brew install efm-langserver yabai shkd borders
+brew install --cask alacritty
+brew upgrade
