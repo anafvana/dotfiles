5db57e30579e6117d37b205ad271e04bcef7ba1a
Ana <56302061+anafvana@users.noreply.github.com>
2025-02-10 07:30:14 +0100

    👌 IMPROVE: Generalise setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,177 +1,125 @@
 #!/bin/bash
 
+# HANDLE DOTFILES PATH #
 DOTFILES=$HOME/.dotfiles
-HOMEFILES=$HOME/.dotfiles/home
-CONFIGFILES=$DOTFILES/.config
-SCRIPTS=$HOME/.dotfiles/scripts
+# Derived variables (assigned after path is confirmed): $CONFIGFILES, $HOMEFILES, $SCRIPTS
 
-#checking for correct paths
-read -r -p "The path of dotfiles is set at $DOTFILES . Is this correct? [Y]es [n]o: " answer
-answer=$(echo "$answer" | awk '{print tolower($0)}')
-
-if [[ $answer == "n" || $answer == "no" ]]
-then
+getNewDotfiles() {
 	read -r -p "What is the correct path? " path
 	DOTFILES=$path
-	HOMEFILES=$DOTFILES/home
-	CONFIGFILES=$DOTFILES/.config
-	SCRIPTS=$DOTFILES/scripts
-fi
+}
 
-echo "The following paths within THIS GIT REPOSITORY have been set:"
-sleep 1
-echo "Scripts: $SCRIPTS"
-sleep 1
-echo "Home files: $HOMEFILES"
-sleep 1
-echo "Config files: $CONFIGFILES"
-sleep 1
-read -r -p "Is this correct? [Y]es [n]o: " answer
-answer=$(echo "$answer" | awk '{print tolower($0)}')
-
-if [[ $answer == "n" || $answer == "no" ]]
-then
-	read -r -p "What is the correct path for SCRIPTS? [ENTER to skip]" scriptpath
-	if [[ ! $scriptpath == "" ]]
-	then
-		SCRIPTS=$scriptpath
+# Validate and confirm dotfiles path
+while true; do
+	# Validate path
+	if [ ! -d "$DOTFILES" ]; then
+		echo "Could not locate .dotfiles at expected path $DOTFILES"
+		getNewDotfiles
 	fi
 
-		read -r -p "What is the correct path for HOME FILES? [RETURN to skip]" homepath
-	if [[ ! $homepath == "" ]]
-	then
-		HOMEFILES=$homepath
+	# Confirm path
+	read -r -p "The path of dotfiles is set at $DOTFILES. Is this correct? [Y]es [n]o: " answer
+	answer=$(echo "$answer" | awk '{print tolower($0)}')
+
+	if [[ $answer == "n" || $answer == "no" ]]; then
+		getNewDotfiles
+	else
+		HOMEFILES=$DOTFILES/home
+		CONFIGFILES=$DOTFILES/.config
+		SCRIPTS=$DOTFILES/scripts
+		break
 	fi
-
-		read -r -p "What is the correct path for CONFIG FILES? [RETURN to skip]" configpath
-	if [[ ! $configpath == "" ]]
-	then
-		CONFIGFILES=$configpath
-	fi
-fi
-
-# searching for variables and adding/replacing
-cntdf=$(grep -c "DOTFILES=" "$HOMEFILES/.bashrc")
-if [ $cntdf -gt 0 ]
-then
-	echo "Updating DOTFILES global variable"
-	sed -i "s|DOTFILES=.*|DOTFILES=${DOTFILES}|" "$HOMEFILES/.bashrc"
-else
-	echo "Creating DOTFILES global variable"
-	sed -i "1i export DOTFILES=${DOTFILES}" "$HOMEFILES/.bashrc"
-fi
-
-cnthf=$(grep -c "HOMEFILES=" "$HOMEFILES/.bashrc")
-if [ $cnthf -gt 0 ]
-then
-	echo "Updating HOMEFILES global variable"
-	sed -i "s|HOMEFILES=.*|HOMEFILES=${HOMEFILES}|" "$HOMEFILES/.bashrc"
-else
-	echo "Creating HOMEFILES global variable"
-	sed -i "1i export HOMEFILES=${HOMEFILES}" "$HOMEFILES/.bashrc"
-fi
-
-cntsc=$(grep -c "SCRIPTS=" "$HOMEFILES/.bashrc")
-if [ $cntsc -gt 0 ]
-then
-	echo "Updating SCRIPTS global variable"
-	sed -i "s|SCRIPTS=.*|SCRIPTS=${SCRIPTS}|" "$HOMEFILES/.bashrc"
-else
-	echo "Creating SCRIPTS global variable"
-	sed -i "1i export SCRIPTS=${SCRIPTS}" "$HOMEFILES/.bashrc"
-fi
-
-cntcf=$(grep -c "CONFIGFILES=" "$HOMEFILES/.bashrc")
-if [ $cntcf -gt 0 ]
-then
-	echo "Updating CONFIGFILES global variable"
-	sed -i "s|CONFIGFILES=.*|CONFIGFILES=${CONFIGFILES}|" "$HOMEFILES/.bashrc"
-else
-	echo "Creating CONFIGFILES global variable"
-	sed -i "1i export CONFIGFILES=${CONFIGFILES}" "$HOMEFILES/.bashrc"
-fi
-
-# DISTRO-DEPENDANT INSTALLATIONS #
-pkg=$(bash "$SCRIPTS/utils/id_pkgmgr.sh")
-case $pkg in
-	apt)
-		bash "$SCRIPTS/utils/setup-apt.sh"
-		;;
-	dnf)
-		bash "$SCRIPTS/utils/setup-dnf.sh"
-		;;
-	*)
-		echo "No setup for package manager: $pkg"
-		;;
-esac
-
-# SYMLINKS #
-## bashrc
-if [ -f "$HOME/.bashrc" ]  || [ -L "$HOME/.bashrc" ]
-then
-	mv .bashrc .bashrc_old
-fi
-
-ln -s "$HOMEFILES/.bashrc" --target-directory="$HOME"
-source "$HOME/.bashrc"
-
-## .config files
-# format: "target" "subdirectory_in_.config"
-SYMLINKS=(
-#	"bspwm" ""
-#	"sxhkd" ""
-	"nvim" ""
-)
-
-for (( i=0;i<${#SYMLINKS[@]};i++))
-do
-	TARGET=$CONFIGFILES/${SYMLINKS[$i]}
-	j=$((i + 1)) #fetch .config sublocation
-	DIRECT=$HOME/.config/${SYMLINKS[$j]}
-
-	#check if .config exists
-	chmod +x "$TARGET"
-	if [[ ! -d "$DIRECT" ]]
-	then
-		mkdir -p "$DIRECT"
-	fi
-
-	#check if symlink already exists (and back it up)
-	if [ -d "$DIRECT/${SYMLINKS[$i]}" ] || [ -L "$DIRECT/${SYMLINKS[$i]}" ]
-	then
-		echo "A symlink for ${SYMLINKS[$i]} was found. Backing it up to ${SYMLINKS[$i]}_old"
-		mv "$DIRECT/${SYMLINKS[$i]}" "$DIRECT/${SYMLINKS[$i]}_old"
-	fi
-	ln -s "$TARGET" "$DIRECT"
-
-	#skip next i (.config sublocation)
-	i=$((i + 1))
 done
 
-## Touchpad config
-#tpconf="30-touchpad.conf"
-#if [ -L /usr/share/X11/xorg.conf.d/${tpconf} ]
-#then
-#	echo "A symlink for ${tpconf} was found. Backing it up to ${tpconf}_old"
-#	sudo mv /usr/share/X11/xorg.conf.d/${tpconf} /usr/share/X11/xorg.conf.d/${tpconf}_old
-#fi
-#sudo ln -s "$DOTFILES/other/xorg.conf.d/30-touchpad.conf" /usr/share/X11/xorg.conf.d
+if [ ! -d "$SCRIPTS" ] || [ ! -d "$HOMEFILES" ] || [ ! -d "$CONFIGFILES" ]; then
+	echo "One or more directories are missing:"
+	[ ! -d "$SCRIPTS" ] && echo "  $SCRIPTS"
+	[ ! -d "$HOMEFILES" ] && echo "  $HOMEFILES"
+	[ ! -d "$CONFIGFILES" ] && echo "  $CONFIGFILES"
+	echo "Quitting..."
+	exit 1
 
-# INSTALLATIONS #
-## eww config
-#bash "$SCRIPTS/eww.sh"
+fi
 
-## golang
-#bash "$SCRIPTS/go.sh"
+# FOR CHANGING THE FOLDER STRUCTURE AND REFLECTING ON .bashrc
+allowChangeStructure() {
+	echo "The following paths within THIS GIT REPOSITORY have been set:"
+	sleep 1
+	echo "Scripts: $SCRIPTS"
+	sleep 1
+	echo "Home files: $HOMEFILES"
+	sleep 1
+	echo "Config files: $CONFIGFILES"
+	sleep 1
+	read -r -p "Is this correct? [Y]es [n]o: " answer
+	answer=$(echo "$answer" | awk '{print tolower($0)}')
 
-## nvim plugins
-bash "$SCRIPTS/nvim-setup.sh"
+	echo "Answer is $answer"
 
-## SETTINGS #
-### Brightness without sudo
-#sudo chown root:root /usr/bin/brightnessctl
-#sudo chmod 4775 /usr/bin/brightnessctl
+	if [[ $answer == "n" || $answer == "no" ]]
+	then
+		read -r -p "What is the correct path for SCRIPTS? [ENTER to skip]" scriptpath
+		if [[ ! $scriptpath == "" ]]
+		then
+			SCRIPTS=$scriptpath
+		fi
 
-echo
-echo "Setup done."
-echo "Go to $HOME/.bashrc and check that all exported paths are correct."
+			read -r -p "What is the correct path for HOME FILES? [RETURN to skip]" homepath
+		if [[ ! $homepath == "" ]]
+		then
+			HOMEFILES=$homepath
+		fi
+
+			read -r -p "What is the correct path for CONFIG FILES? [RETURN to skip]" configpath
+		if [[ ! $configpath == "" ]]
+		then
+			CONFIGFILES=$configpath
+		fi
+	fi
+
+	# searching for variables and adding/replacing
+	cntdf=$(grep -c "DOTFILES=" "$HOMEFILES/.bashrc")
+	if [ $cntdf -gt 0 ]
+	then
+		echo "Updating DOTFILES global variable"
+		sed -i "s|DOTFILES=.*|DOTFILES=${DOTFILES}|" "$HOMEFILES/.bashrc"
+	else
+		echo "Creating DOTFILES global variable"
+		sed -i "1i export DOTFILES=${DOTFILES}" "$HOMEFILES/.bashrc"
+	fi
+
+	cnthf=$(grep -c "HOMEFILES=" "$HOMEFILES/.bashrc")
+	if [ $cnthf -gt 0 ]
+	then
+		echo "Updating HOMEFILES global variable"
+		sed -i "s|HOMEFILES=.*|HOMEFILES=${HOMEFILES}|" "$HOMEFILES/.bashrc"
+	else
+		echo "Creating HOMEFILES global variable"
+		sed -i "1i export HOMEFILES=${HOMEFILES}" "$HOMEFILES/.bashrc"
+	fi
+
+	cntsc=$(grep -c "SCRIPTS=" "$HOMEFILES/.bashrc")
+	if [ $cntsc -gt 0 ]
+	then
+		echo "Updating SCRIPTS global variable"
+		sed -i "s|SCRIPTS=.*|SCRIPTS=${SCRIPTS}|" "$HOMEFILES/.bashrc"
+	else
+		echo "Creating SCRIPTS global variable"
+		sed -i "1i export SCRIPTS=${SCRIPTS}" "$HOMEFILES/.bashrc"
+	fi
+
+	cntcf=$(grep -c "CONFIGFILES=" "$HOMEFILES/.bashrc")
+	if [ $cntcf -gt 0 ]
+	then
+		echo "Updating CONFIGFILES global variable"
+		sed -i "s|CONFIGFILES=.*|CONFIGFILES=${CONFIGFILES}|" "$HOMEFILES/.bashrc"
+	else
+		echo "Creating CONFIGFILES global variable"
+		sed -i "1i export CONFIGFILES=${CONFIGFILES}" "$HOMEFILES/.bashrc"
+	fi
+}
+# allowChangeStructure
+
+
+# ADDITIONAL SOFTWARE #
+bash "$SCRIPTS/utils/setup-packages.sh"
