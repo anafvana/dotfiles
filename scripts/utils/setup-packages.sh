86f232dd7b496ccfbea9e890155b92863976c3cd
Ana <56302061+anafvana@users.noreply.github.com>
2025-02-10 05:58:10 +0100

    👌 IMPROVE: setup-packages using python script - FAILING

--- scripts/utils/setup-packages.sh
+++ scripts/utils/setup-packages.sh
@@ -5,25 +5,43 @@ setup_linux() {
 	pkg=$(bash "$UTILS/id_pkgmgr.sh")
 	case $pkg in
 	   apt)
-	       #bash "$UTILS/setup-apt.sh"
-	       echo "apt"
+		   sudo apt update
+	       sudo apt install python3 python3-pip python3-venv
 		   ;;
 	   dnf)
-	       bash "$UTILS/setup-dnf.sh"
+		   sudo dnf update
+		   sudo dnf install python3 python3-pip python3-venv
 	       ;;
 	   *)
 	       echo "No setup for package manager: $pkg"
+		   exit 1
 	       ;;
 	esac
 }
 
 case "$os" in
 	Darwin)
-		bash "$UTILS/setup-mac.sh"
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		brew install python
+		python3 -m pip install virtualenv
+		pkg="brew"
 		;;
 	Linux)
 		setup_linux
 		;;
 	*)
 		echo "No setup for OS: $os"
+		exit 1
+		;;
 esac
+
+echo `which python`
+cd "$UTILS/setup-packages"
+unalias venv
+if [ ! -d "$(pwd)/venv" ]; then python3 -m venv "$(pwd)/venv"; fi; source "$(pwd)/venv/bin/activate"
+python -m pip install -r requirements.txt
+chmod +x setup-packages.py
+tmpScript=$(python "./setup-packages.py" "$pkg")
+cd "$SCRIPTS"
+chmod +x tmpScript
+echo "In bash $tmpScript"
