aee7b8e503933094e20171e89530755af2db12f6
A <56302061+anafvana@users.noreply.github.com>
2025-04-17 03:15:52 +0200

    📦 NEW: Bash 5.4 installation for mac

--- scripts/utils/setup-mac.sh
+++ scripts/utils/setup-mac.sh
@@ -5,6 +5,33 @@ echo "Symlinking .zshrc and .bash_profile"
 rm "$HOME/.zshrc" &> /dev/null; ln -s "$HOME/.bashrc" "$HOME/.zshrc"
 rm "$HOME/.bash_profile" &> /dev/null; ln -s "$HOME/.bashrc" "$HOME/.bash_profile"
 
+
+# Install newest bash
+echo "Installing newest bash"
+brew install bash
+brew link bash
+newBash="$(brew --prefix)/bin/bash"
+
+# Add new login shell if not present
+if ! grep -q "$newBash" /etc/shells; then
+  sudo awk -v newBash="$newBash" '
+    # Print the comments (lines starting with #)
+    /^#/ { print; next }
+
+    # When an empty line is found, print it, then add $newBash
+    /^[[:space:]]*$/ && !added {
+      print
+      print newBash
+      added = 1
+      next
+    }
+
+    # Print everything else
+    { print }
+  ' /etc/shells > /tmp/shells.tmp && sudo mv /tmp/shells.tmp /etc/shells && rm /tmp/shells.tmp
+
+fi
+
 # Install brew
 echo "Installing brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
